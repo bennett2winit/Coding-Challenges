00c8e9801b14cc6e4416a1db63779a20cb455cf4
Blake Bennett <blakbenn@gmail.com>
2024-09-28 19:40:55 -0500

    Started the initial structure of the JSON Parser. Currently it will test whether the JSON is actually a JSON

---
+++ JSON_Parser/jsParse.py
@@ -0,0 +1,29 @@
+import sys
+
+fileContent = ''
+for line in sys.stdin:
+    fileContent += line 
+
+
+def validCheck(file):
+    opened = False
+    closed = False
+    for i in fileContent:
+        if i == '{':
+            opened = True
+            closed = False
+        elif i == '}' and opened == False:
+            return 1
+        elif i == '}' and opened == True:
+            closed = True
+            opened = False
+    if opened == False and closed == True:
+        return 0
+    else:
+        return 2
+print(validCheck(fileContent))
+        
+'''
+1 = file doesn't start with a {
+'''
+

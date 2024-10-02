04bb4af0b97619592edd1e122e0b0e8440cb4c52
bennett2winit <blakbenn@gmail.com>
2024-10-02 11:31:30 -0500

    Added support for boolean and integer values

---
+++ JSON_Parser/intTest.py
@@ -0,0 +1,7 @@
+i = ' 2'
+s = 's'
+
+print(type(i))
+print(type(s))
+print(int(i))
+print(int(s))
--- JSON_Parser/jsParse.py
+++ JSON_Parser/jsParse.py
@@ -6,11 +6,11 @@ for line in sys.stdin:
 
 def main(file):
     bracket = bracketCheck(file)
-    keyVal = keyCheck(file)
+    keyValid = keyCheck(file)
     if bracket > 0:
         print('invalid JSON file: ' + str(bracket))
-    elif keyVal > 0:
-        print('invalid JSON file: ' + str(keyVal))
+    elif keyValid > 0:
+        print('invalid JSON file: ' + str(keyValid))
     else:
         print("Valid JSON")
 
@@ -34,32 +34,80 @@ def bracketCheck(file):
 def keyCheck(file):
     key = False
     value = False
-    quoteCounter = 0
+    keyVal = ''
+    valueVal = ''
     # Iterate through file to check for key and value combos. ',' resets them so that we can check for multiple values
     for i in file:
-        if i == '"':
-            quoteCounter += 1
-        elif quoteCounter > 2:
-            return 3
-        elif quoteCounter == 2 and i == ':':
-            key = True
-            quoteCounter = 0
-        elif key == True and quoteCounter == 2  and i == '}':
-            value = True
-        elif quoteCounter == 2 and i == ',':
+        if i == ':':
+            keyNum = checkKey(keyVal)
+            if keyNum > 0:
+                key = False
+            else:
+                key = True
+            checkKey(keyVal)
+            keyVal = ''
+        elif i == ',' and key != True:
+            print("no key found")
+        elif i == ',':
+            value = checkValue(valueVal)
+            if value == False:
+                return 6
+            valueVal = ''
             key = False
             value = False
-            quoteCounter = 0
+        elif key == False and i != ':':
+            keyVal += i
+        elif key == True and i == "}":
+            value = checkValue(valueVal)
+            print(value)
+            if value == False:
+                return 6
+                break
+        elif key == True and i != ',':
+            valueVal += i
     if key == True and value == True:
         return 0
     else:
         return 2
-       
+
+def checkValue(val):
+    stringCheck = checkKey(val)
+    if stringCheck == 0:
+        return True
+    elif val.strip() == 'null':
+        return False
+    else:
+        try:
+            int(val)
+            return True
+        except:
+            bool(val)
+            return True
+        else:
+            return False
+    print("check value run")
+def checkKey(val):
+    quoteCounter = 0
+    for i in val:
+        if i == '"':
+            quoteCounter += 1
+    if quoteCounter == 2:
+        return 0
+    elif quoteCounter < 2:
+        return 4
+    elif quoteCounter > 2:
+        return 3
+    
+
+
 main(fileContent)
 
 '''
 1 = file doesn't start with a {
 2 = Undefined
 3 = too many quotes
+4 = no string
+5 = wrong value type
+6 = invalid value
 '''
 
